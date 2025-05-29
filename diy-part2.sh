3e08b5657f7f746242351827480c02d5f2d312b7
jtzx082 <yujq804@gmail.com>
2025-05-29 10:45:06 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -22,4 +22,4 @@
 # 加入turboacc
 curl -sSL https://raw.githubusercontent.com/chenmozhijin/turboacc/luci/add_turboacc.sh -o add_turboacc.sh
 chmod -R 777 add_turboacc.sh
-bash add_turboacc.sh --no-sfe
+bash add_turboacc.sh
